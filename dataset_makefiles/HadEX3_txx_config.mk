b395fdbd0e6561afdc9e331a1f980b55117f61c7
stellema <40450353+stellema@users.noreply.github.com>
2026-04-21 18:32:05 +1000

    Add tasmax HadEX3 dataset config files
    - Converts lon to 0-360 for consistency (using `custom_code` option when fixing metadata)

---
+++ dataset_makefiles/HadEX3_txx_config.mk
@@ -0,0 +1,10 @@
+# Configuration file for Makefile workflows 
+# Downloaded from www.climdex.org
+
+OBS_DATASET=HadEX3
+OBS_LABEL=HadEX3
+# OBS_DATA := /g/data/xv83/unseen-projects/outputs/txx-global/data/HadEX3_monthly_files/HadEX3_ref1961-1990_TXx_*_RawData_1901-2018_-84to84_-180to180.nc
+OBS_DATA := /g/data/xv83/unseen-projects/outputs/txx-global/data/HadEX3_ref1961-1990_TXx_ANN_RawData_1901-2018_-84to84_-180to180.nc
+
+OBS_CONFIG=/g/data/xv83/unseen-projects/code/dataset_config/dataset_hadex3.yml
+OBS_TIME_PERIOD=1901-2018
